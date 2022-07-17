7ed698244bfd11639349990b85d07fdbe61f2c61
James Adams <inklink28@gmail.com>
2022-07-17 10:31:16 -0400

    Update grid import

--- src/cadqueryhelper/grid.py
+++ src/cadqueryhelper/grid.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import cadquery as cq
-import parts
+from cadqueryhelper import parts
 
 def make_grid(part, dim, odd_col_push = [0,0], columns = 5, rows = 5):
     print('attempting to make grid');
