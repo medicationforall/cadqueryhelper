8bf95293f8a1a6c6c1d9eaee9e061270303ada9c
medicationforall <inklink28@gmail.com>
2022-09-02 11:04:36 -0400

    Removed unused arch parameter

--- example/shape/arch.py
+++ example/shape/arch.py
@@ -1,7 +1,7 @@
 import cadquery as cq
 from cadqueryhelper import shape
 
-part = shape.arch_pointed(length=30, width=5, height=50, inner_height=25, inner_width=3)
+part = shape.arch_pointed(length=30, width=5, height=50, inner_height=25)
 cq.exporters.export(part,'out/arch_pointed.stl')
 
 if part.metadata:
